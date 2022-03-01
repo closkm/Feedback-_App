b5701100f360230423d3a462b16b8c43840ed161
Mike <mikeclosky29@gmail.com>
2022-02-28 18:22:19 -0600

    feedback app working with back end functionality

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,20 @@
+const FeedbackData = [
+    {
+      id: 1,
+      rating: 10,
+      text: 'this is a feature, not a bug',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'michael closky',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'closky michael',
+    },
+  ]
+
+
+export default FeedbackData
